c42ee963512ffb6531bc43b062e6abb028a0b771
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-17 18:51:33 -0700

    tests need debuging

--- tests/server_tests.js
+++ tests/server_tests.js
@@ -54,6 +54,7 @@ describe('User data', function() {
 				chai.request('localhost:3000')
 					.get('/api/users/user')
 					.end(function(err, res) {
+						expect(err).to.eql(null);
 						user = res.body;
 						console.log('response body: ' + res.body);
 						done();
@@ -68,16 +69,55 @@ describe('User data', function() {
 			});
 		});
 
-		//Post test
+		//POST test
+		describe('POST method for User', function() {
+			it('creates new user', function(done) {
+				chai.request('localhost:3000')
+					.post('/api/users')
+					.send({userName: 'name'})
+					.end(function(err, res) {
+						expect(err).to.eql(null);
+						expect(res.body.userName).to.eql('name');
+						done();
+					});
+			}); 
+		});
+
+		//PUT test
+		describe('PUT method for User', function() {
+			var response;
+			before(function(done) {
+				chai.request('localhost:3000')
+					.put('/api/users/' + newUser.id)
+					.send({userName: 'name'})
+					.end(function(err, res) {
+						expect(err).to.eql(null);
+						response = res.body;
+						done();
+					});
+			});
+
+			it('should update the user', function() {
+				expect(response).to.eql('success');
+			});
+		});
+
+		//DELETE test
+		describe('DELETE method for User', function() {
+			var response;
+			before(function(done) {
+				chai.request('localhost:3000')
+					.del('/api/users' + newUser.id)
+					.end(function(err, res) {
+						expect(err).to.eql(null);
+						response = res.body;
+						done();
+					});
+			});
+
+			it('responds with message', function() {
+				expect(response).to.eql('success');
+			});
+		});
 	});
 });
-
-
-
-
-
-
-
-
-
-
